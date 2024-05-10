9eea88ec4618e9e33593182710007671f4a5646b
danieleocchi <dennyais146@gmail.com>
2024-05-10 08:36:40 +0200

    first commit

---
+++ src/main/java/co/develhope/mockdatabase/MockdatabaseApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.mockdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockdatabaseApplication.class, args);
+	}
+
+}
